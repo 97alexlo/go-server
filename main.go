e7a092069bc3940152bae4315a24bfeab8d64976
Alex Lo <7253alex@gmail.com>
2024-01-25 19:43:34 -0800

    test

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	// "log"
-	// "net/http"
+	// "net/http"ZZZZZZZZZZZZZZ
 )
 
 func main() {
 	fmt.Println("test")
 }
+
+http.Handle("/foo", fooHandler)
+
+http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+})
+
+
+log.Fatal(http.ListenAndServe(":8080", nil))
+
